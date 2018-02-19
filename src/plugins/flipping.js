883be594e67bfa56be963dd50df7a04d3f172280
Sascha Klatt <coding@klattiation.de>
2018-02-19 23:39:09 +0100

    remove mirrorX() and mirrorY(), update docs

--- src/plugins/flipping.js
+++ src/plugins/flipping.js
@@ -49,33 +49,5 @@ export default function(context, stateProvider) {
         return context;
     }
 
-    /**
-     * Flips the array on the given x-position
-     *
-     * @deprecated Will be removed in version > 0.9.x. Use [flipX()]{@link gridl#flipX}  instead.
-     *
-     * @memberOf gridl
-     * @method
-     * @instance
-     *
-     * @param {number} xPos - The x-position of where to flip the array.
-     * @returns {gridl} The same gridl instance.
-     */
-    const mirrorX = flipX;
-
-    /**
-     * Flips the array on the given y-position.
-     *
-     * @deprecated Will be removed in version > 0.9.x. Use [flipY()]{@link gridl#flipY}  instead.
-     *
-     * @memberOf gridl
-     * @method
-     * @instance
-     *
-     * @param {number} yPos - The y-position of where to flip the array.
-     * @returns {gridl} The same gridl instance.
-     */
-    const mirrorY = flipY;
-
-    return { flipX, flipY, mirrorX, mirrorY };
+    return { flipX, flipY };
 }
