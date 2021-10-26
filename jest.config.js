c516f5ff16d677e051b83e6fec3df30753e0d1f4
Sascha Klatt <coding@klattiation.de>
2021-10-26 22:03:45 +0200

    fix jest haste map warning

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     moduleFileExtensions: ["js", "json", "d.ts", "ts", "node"],
+    modulePathIgnorePatterns: ["<rootDir>/dist"],
     coverageDirectory: "coverage",
     coverageReporters: [
         "json-summary",
